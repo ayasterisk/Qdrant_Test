da8b9e9aec46ccefc43b3da4092b95bdcdb5e0b5
Ayasterisk <ayasterisk@gmail.com>
2026-03-13 11:17:31 +0700

    Update app.py

--- src/app.py
+++ src/app.py
@@ -1,4 +1,4 @@
-import app as st
+import streamlit as st
 from multi_hop_rag import multi_hop_rag
 
 st.title("Multi-hop RAG QA")
@@ -6,7 +6,5 @@ st.title("Multi-hop RAG QA")
 question = st.text_input("Ask a question")
 
 if question:
-
     answer = multi_hop_rag(question)
-
     st.write(answer)
