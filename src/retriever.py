c153e17271739f880560f0d9139c5cbde10dd915
Ayasterisk <ayasterisk@gmail.com>
2026-03-13 12:29:20 +0700

    Update retriever.py

--- src/retriever.py
+++ src/retriever.py
@@ -1,14 +1,22 @@
 from qdrant_client import QdrantClient
 from embedder import embed
 from config import COLLECTION_NAME, TOP_K
+import os
 
-client = QdrantClient("localhost", port=6333)
+
+# Kết nối Qdrant
+client = QdrantClient(
+    url=os.getenv("QDRANT_URL"),
+    api_key=os.getenv("QDRANT_API_KEY")
+)
 
 
 def retrieve(question):
 
+    # embedding câu hỏi
     vector = embed(question)
 
+    # search vector
     results = client.query_points(
         collection_name=COLLECTION_NAME,
         query=vector,
