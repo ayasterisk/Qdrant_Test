eb3c4c437fa044e25464197c8652037a4278b883
Ayasterisk <ayasterisk@gmail.com>
2026-03-13 13:54:23 +0700

    ingest

--- scripts/ingest_qdrant.py
+++ scripts/ingest_qdrant.py
@@ -1,4 +1,7 @@
+import sys
 import os
+
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
 from datasets import load_from_disk
 
 from qdrant_client import QdrantClient
